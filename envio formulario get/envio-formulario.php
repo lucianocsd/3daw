b7a9960c76ee2398bc5126dbb4f6c7fde8b455fb
lucianocsd <89948204+lucianocsd@users.noreply.github.com>
2021-10-06 19:30:09 -0300

    Envio via GET

---
+++ envio formulario get/envio-formulario.php
@@ -0,0 +1,15 @@
+<?php
+$name = $_GET["name"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>envio-formulario</title>
+
+</head>
+<body>
+    <h1> <?php echo "O nome é $name"; ?></h1>
+</body>
+
+</html>
